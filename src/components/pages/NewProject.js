02b97d14ce2fd8ab5289b45a34ca199d29f03893
Davi Angelo <davi.magalhaes.pb@compasso.com.br>
2024-07-26 16:48:47 -0300

    Adição do formulario de criação de projetos

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,7 +1,12 @@
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProject.module.css'
+
 function NewProject() {
     return (
-        <div>
-            <h1>Novo Projeto</h1>
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para então adicionar os serviços</p>
+            <ProjectForm />
         </div>
     )
 }
