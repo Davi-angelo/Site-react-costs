7c55a1de764968d59ef3f3651b969db96811faf1
Davi Angelo <davi.magalhaes.pb@compasso.com.br>
2024-08-01 23:46:20 -0300

    Teste de interface dinamica com busca em api

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
 
+import Container from "../layout/Container";
+
+import styles from './Contact.module.css'
+
 function Contact() {
     const [count, setCount] = useState(0)
 
@@ -9,11 +13,41 @@ function Contact() {
         }, 1000)
     })
 
+    const [imagens, setImagens] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.thecatapi.com/v1/images/search?limit=10', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+
+        }).then((resp) => resp.json())
+            .then((data) => {
+                setImagens(data)
+            })
+            .catch((err) => console.log(err))
+
+    }, [])
+
 
     return (
         <div>
             <h1>Contato {count}</h1>
+            <div className="{}">
+                <div className="{}">
+                    <h1>Minhas Imagens</h1>
+                </div>
+                <Container customClass="start">
+                    {imagens.length > 0 &&
+                        imagens.map((imagem) => (
+                            <img className={styles.card} src={imagem.url}  key={imagem.id}/>
+                        ))}
+
+                </Container>
+            </div>
         </div>
+
     )
 }
 
